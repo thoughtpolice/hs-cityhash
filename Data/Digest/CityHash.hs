359003c0fc120c86332d797ddfe6710dc0b2b486
Austin Seipp <mad.one@gmail.com>
2012-02-05 08:56:16 -0600

    Fix maintainer email

--- Data/Digest/CityHash.hs
+++ Data/Digest/CityHash.hs
@@ -4,7 +4,7 @@
 -- Copyright   : (c) Austin Seipp 2011
 -- License     : BSD3
 -- 
--- Maintainer  : as@hacks.yi.org
+-- Maintainer  : mad.one@gmail.com
 -- Stability   : experimental
 -- Portability : portable (FFI)
 -- 
